7b50679167b096eec6416c94d6058963ec0ce2bf
shayRusso320 <66921237+shayRusso320@users.noreply.github.com>
2021-04-20 17:39:34 +0300

    Create main.py
    created a program for shrinking a string

---
+++ main.py
@@ -0,0 +1,17 @@
+def shrink_string(st):
+    new_string = ""
+    lastChar = ''
+    reps = 0
+    for c in st:
+        if lastChar != c:
+            if lastChar != '':
+                new_string += lastChar + str(reps)
+            lastChar = c
+            reps = 1
+        else:
+            reps += 1
+    return new_string  
+            
+string = 'aabbbbcdddeaaaaa'
+print(string)
+print(shrink_string(string))
